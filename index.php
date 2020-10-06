d5afe42e8da21a9919b1912aaa714422175a2ba4
phasawee371 <68213815+phasawee371@users.noreply.github.com>
2020-10-06 10:27:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,64 @@
-<!DOCTYPE html>
+<!-- Preloading is an optional speedup -->
+<link href="https://cobrowse.screenmeet.com/CobrowseCore.js" rel="preload" as="script"/>
+
+<!-- Required --> 
+<script type="text/javascript" async="true" src="https://cobrowse.screenmeet.com/CobrowseBootstrap.js"></script>
+<script type="text/javascript" >
+window.Cobrowse = window.Cobrowse || {};
+window.Cobrowse.modal = window.Cobrowse.modal || {
+  commandQueue:[],
+  useHotkeys: true
+};
+
+
+
+</script>
+
 <html>
-  <head>
-    <title></title>
-    <script src="https://cobrowse.screenmeet.com/videoAssist.js"></script>
-    <link rel="stylesheet" href="https://cobrowse.screenmeet.com/videoAssist.css" type="text/css" />
-  </head>
-  <body>
-    <div id="screenMeetLiveContainer">
-      <div id="screenMeetLiveContainerHeader"></div>
-      <iframe id="screenMeetLivePortal" allow="camera *;microphone *"></iframe>
-    </div>
-    <iframe id="screenMeetCobrowseContainer" src=""></iframe>
-    <iframe
-      id="cbSessionCarrier"
-      width="1"
-      height="1"
-      style="display: none"
-      referrerpolicy="origin"
-      src="https://edge.screenmeet.com/v3/cobrowse/multidomain"
-    ></iframe>
-    <div id="screenMeetClickMask"></div>
-  </body>
+<body>
+Hello!! <p/>
+Let's Cobrowse
+<button id="myBtn" onclick="window.Cobrowse.modal.openModal();">Co Browse</button>
+<p/>
+<table>
+
+<tr>
+
+<table>
+<tr>
+<td>Account No:</td><td><div id="accountnumber" data-cb-mask="true"> 23434645631234523</div></td>
+</tr>
+
+<tr>
+<td>Credit Card No:</td><td> <input type="string" id="credit-card-number" data-cb-mask="true" /></td>
+</tr>
+</table>
+
+</tr>
+<tr>
+<table>
+<tr>
+<td>
+<div id="freetext" height="5000px" background-color="blue">
+<h1>Adding text to scroll here....</h1></div>
+<div class="content">
+ <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+</div>
+<h1>End of Page....</h1></div>
+</td>
+</tr>
+</table>
+</tr>
+
+</body>
 </html>
