a7f607452f4a6072c01c66822b9eb00a14241f78
phasawee371 <68213815+phasawee371@users.noreply.github.com>
2020-10-12 10:54:38 +0700

    Update screenmeetvdo.php

--- screenmeetvdo.php
+++ screenmeetvdo.php
@@ -14,8 +14,8 @@
   box-shadow: 0px 0px 40px -5px rgba(0,0,0,0.6);
   transition-property: width, height, top, left, transform;
   transition-duration: .5s;
-  width: 450px;
-  height: 520px;
+  width: 350px;
+  height: 420px;
 }
 
 #screenMeetLiveContainer.cb-videoclosed {
